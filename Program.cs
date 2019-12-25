a56ed891c5715b70fe06c22a11f8a4c803f92a9d
rummolprod999 <rummolprod999@gmail.com>
2019-12-25 14:00:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharp8Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
